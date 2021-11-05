3c40c9268eeba64f68a4fa58bba908c118c89d27
Furkan Uykun <31152577+fuykun@users.noreply.github.com>
2021-11-05 15:53:43 +0300

    Add files via upload

---
+++ AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthApi.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthApi.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly ILogger<AuthController> _logger;
+
+		public AuthController(ILogger<AuthController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public ActionResult<string> Get()
+		{
+			_logger.LogInformation("get");
+			return "UP!";
+		}
+
+		[Route("user")]
+		[HttpPost]
+		public async Task<IActionResult> CheckUser([FromForm] UserAuthRequest request)
+		{
+			var tags = new[] { "administrator", "management", "monitoring" };
+
+			if (request.Username == "admin" && request.Password == "admin")
+			{
+				return AuthResult.Allow(tags);
+			}
+			else
+			{
+				return AuthResult.Deny();
+			}
+		}
+
+		[Route("vhost")]
+		[HttpPost]
+		public IActionResult CheckVhost([FromForm] VhostAuthRequest request)
+		{
+			var userlog = $"user : {request.Username}, ip : {request.Ip}";
+			_logger.LogInformation(userlog);
+			return AuthResult.Allow();
+		}
+
+		[Route("resource")]
+		[HttpPost]
+		public IActionResult CheckResource([FromForm] ResourceAuthRequest request)
+		{
+			var userlog = $"user : {request.UserName}, vhost : {request.Vhost}, resource : {request.Resource}, name : {request.Name}, permission : {request.Permission}";
+			_logger.LogInformation(userlog);
+			return AuthResult.Allow();
+		}
+
+		[Route("topic")]
+		[HttpPost]
+		public IActionResult CheckTopic([FromForm] TopicAuthRequest request)
+		{
+			var userlog =
+				$"user : {request.UserName}, vhost : {request.Vhost}, resource : {request.Resource}, name : {request.Name}, routing key: {request.RoutingKey}, permission : {request.Permission}";
+			_logger.LogInformation(userlog);
+			return AuthResult.Allow();
+		}
+
+		private class AuthResult
+		{
+			public static IActionResult Allow()
+			{
+				return new OkObjectResult("allow");
+			}
+
+			public static IActionResult Allow(params string[] tags)
+			{
+				return new OkObjectResult($"allow {string.Join(" ", tags)}");
+			}
+
+			public static IActionResult Deny()
+			{
+				return new OkObjectResult("deny");
+			}
+		}
+	}
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMqTestApp
+{
+	class Program
+	{
+		private const string HostName = "localhost";
+		private const string UserName = "admin";
+		private const string Password = "admin";
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine($"Hostname: {HostName} | Username: {UserName} | Password: {Password}");
+				Send();
+				Console.WriteLine($"RabbitMQ mesaj gönderme işlemi başarılı.");
+				Console.ReadLine();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"RabbitMQ mesaj gönderme işlemi başarısız sonuçlandı.");
+				throw;
+			}
+		}
+		private static void Send()
+		{
+			var factory = new ConnectionFactory() { HostName = HostName, UserName = UserName, Password = Password };
+			using (var connection = factory.CreateConnection())
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+					var message = "Hello World!";
+					var body = Encoding.UTF8.GetBytes(message);
+					channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+					Console.WriteLine(" [x] Sent {0}", message);
+				}
+		}
+	}
+}
